d5788dc6536863ab0d12afacd0b38dce39a3e169
Allen Garvey <allengarvey@hotmail.com>
2015-10-13 11:51:56 -0400

    reorganize and add alias for grep with colors

--- aliases.bash
+++ aliases.bash
@@ -1,14 +1,32 @@
+#SETTINGS
+
+#alias for bash settings
+alias settings="subl ~/.bash_profile \"${DOTFILES_DIR}aliases.bash\""
+
+#BUILT INS
+
+alias grepc="grep -nr --color=always"
+
+#CUSTOM SCRIPTS
+
 #alias for sassquatch
 alias sasq="ruby \"${HOME}/Documents/Programming/Ruby/sassquatch/sassquatch.rb\""
 
 #alias for synced
 alias synced="ruby \"${HOME}/Documents/Programming/Ruby/synced/synced.rb\""
 
+#SUBLIME
+
+#function to create file if not exists and open with sublime
+function edit() { touch ./$1; subl ./$1; }
+
 #alias for sublime
 alias subl="open -a \"Sublime Text 3\""
 
 #alias for sublime packages
 alias subl_settings="cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/sublime-snippets; open ."
+
+#Directories
 
 #function to cd into directory in sites dir
 function sites() { cd ~/Sites/; cd ./$1; }
@@ -22,11 +40,13 @@ function doc() { cd ~/Documents/; cd ./$1; }
 #function to cd into directory in programming dir
 function prog() { cd ~/Documents/Programming/; cd ./$1; }
 
-#alias for bash settings
-alias settings="subl ~/.bash_profile \"${DOTFILES_DIR}aliases.bash\""
-
 #alias to open current directory in finder
 function op() { open ./$1; }
+
+#Git
+
+#alias to open current directory in gitup
+alias gitup="open . -a \"GitUp\""
 
 #function to create git repository and default .gitignore
 function git_setup() { git init; echo -e ".DS_Store\n*.sublime-project\n*.sublime-workspace\n**/.sass-cache/*\n.sass-cache\n.sass-cache/*\nnode_modules/*\n" >> .gitignore; cat "${DOTFILES_DIR}license.txt" >> license.txt; }
@@ -37,11 +57,7 @@ alias g_a="git add .; git status;"
 #function to copy git repo and gitignore
 function git_copy() { cp -r ./.git $1/.git; cp ./.gitignore $1/.gitignore; }
 
-#function to create file if not exists and open with sublime
-function edit() { touch ./$1; subl ./$1; }
-
-#alias to open current directory in gitup
-alias gitup="open . -a \"GitUp\""
+#RAILS
 
 #rails aliases
 alias rls="./bin/rails"
