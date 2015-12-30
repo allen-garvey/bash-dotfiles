e137add317ff40c3c1d95b5adbf4cd83ed4c12b6
Allen Garvey <allen@allengarvey.com>
2015-12-30 18:14:41 -0500

    fix bug in settings alias

--- aliases_ubuntu.bash
+++ aliases_ubuntu.bash
@@ -6,4 +6,4 @@ source "${DOTFILES_DIR}aliases_shared.bash"
 alias grepc="grep -nrP --color=always"
 
 #alias for bash settings
-alias settings="subl ~/.bash_aliases ~/.bashrc ~/.vimrc \"./${DOTFILES_DIR}\""
+alias settings="subl ~/.bash_aliases ~/.bashrc ~/.vimrc \"${DOTFILES_DIR}aliases_ubuntu.bash\" \"${DOTFILES_DIR}aliases_shared.bash\""
