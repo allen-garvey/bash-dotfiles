2b4f1b39c3f6b729719cc5b9b5e49c5bfa626318
Allen Garvey <allengarvey@hotmail.com>
2016-08-15 23:41:17 -0400

    Rename function to grep in files matching extension

--- aliases_shared_footer.bash
+++ aliases_shared_footer.bash
@@ -5,4 +5,4 @@
 #grep for pattern in files with extension
 #http://stackoverflow.com/questions/12516937/grep-but-only-certain-file-extensions
 # $1 pattern $2 - directory $3 file extension
-function grepex() { grepc $1 $2 --include \*.$3;  }
+function grepx() { grepc $1 $2 --include \*.$3;  }
