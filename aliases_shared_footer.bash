f34af05ccaa7c852d79886b201eb1e1eb0638995
Allen Garvey <allengarvey@hotmail.com>
2016-08-15 23:39:12 -0400

    Add function to only grep in files with certain extension

---
+++ aliases_shared_footer.bash
@@ -0,0 +1,8 @@
+#Shared aliases that have to be declared after OS specific aliases
+
+#BUILT INS
+
+#grep for pattern in files with extension
+#http://stackoverflow.com/questions/12516937/grep-but-only-certain-file-extensions
+# $1 pattern $2 - directory $3 file extension
+function grepex() { grepc $1 $2 --include \*.$3;  }
