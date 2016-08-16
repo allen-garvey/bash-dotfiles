e48027ee75c2b5ef19e2c3e2225dfb4765194212
Allen Garvey <allengarvey@hotmail.com>
2016-08-16 16:39:23 -0400

    Add basic aliases for freebsd

---
+++ aliases_freebsd.bash
@@ -0,0 +1,15 @@
+#aliases and functions fro freebsd
+
+export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+export PAGER=less
+export EDITOR=vim
+
+source "${DOTFILES_DIR}aliases_shared.bash"
+
+#built ins
+alias grepc="grep -nrP --color=always"
+
+
+source "${DOTFILES_DIR}aliases_shared_footer.bash"
+
+
