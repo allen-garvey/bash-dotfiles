4c946d52699140622f6350f69cd12eabc7243a69
Allen Garvey <allengarvey@hotmail.com>
2016-08-19 17:25:43 -0400

    Remove unsupported -P flag from bsd grep alias

--- aliases_freebsd.bash
+++ aliases_freebsd.bash
@@ -5,7 +5,7 @@ export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
 LS_COLORS=$LS_COLORS:'di=0;35:' ; export LS_COLORS
 export PAGER=less
 export EDITOR=vim
-alias grepc="grep -nrP --color=always"
+alias grepc="grep -nr --color=always"
 
 source "${DOTFILES_DIR}aliases_shared.bash"
 
