382aa7b649bfacf65d06dea24c710a05e63b44dc
Allen Garvey <allengarvey@hotmail.com>
2016-08-21 22:48:58 -0400

    Move OS comment to top of file

--- aliases_ubuntu.bash
+++ aliases_ubuntu.bash
@@ -1,9 +1,9 @@
+#Aliases for Ubuntu
+
 #built ins declared before shared
 alias grepc="grep -nrP --color=always"
 
 source "${DOTFILES_DIR}aliases_shared.bash"
-
-#Aliases for Ubuntu only
 
 #alias for bash settings
 alias settings="subl ~/.bash_aliases ~/.bashrc ~/.vimrc \"${DOTFILES_DIR}aliases_ubuntu.bash\" \"${DOTFILES_DIR}aliases_shared.bash\""
