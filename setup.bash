2f2e2f6c35312d689de6e5cd58433405ebadeb97
Allen Garvey <allen.garvey@gmail.com>
2016-11-09 22:24:34 -0500

    Fix setup script to not expand variables

--- setup.bash
+++ setup.bash
@@ -3,4 +3,4 @@
 cd ~ & \
 git clone https://github.com/allen-garvey/bash-dotfiles.git bash_dotfiles & \
 touch .bash_profile & \
-echo -e "DOTFILES_DIR=\"${HOME}/bash_dotfiles/\";\nexport DOTFILES_DIR;\nDOTFILES_DIR=\"${HOME}/bash_dotfiles/\";\nexport DOTFILES_DIR;" >> .bash_profile;
+echo -e "DOTFILES_DIR=\"\${HOME}/bash_dotfiles/\";\nexport DOTFILES_DIR;\nDOTFILES_DIR=\"\${HOME}/bash_dotfiles/\";\nexport DOTFILES_DIR;" >> .bash_profile;
