76f0c3453f4f2767a3fad6571ab00f374e88598e
Allen Garvey <allen.garvey@gmail.com>
2016-12-07 22:41:43 -0500

    Add alias to connect to alaska server using ssh

--- aliases_shared.bash
+++ aliases_shared.bash
@@ -169,6 +169,11 @@ alias jeks="jekyll serve --port 3000"
 #REDIS
 alias redis="redis-cli"
 
+#SSH logins
+
 #OSU
 alias flip="ssh garveya@access.engr.oregonstate.edu"
+
+#Alaska Server
+alias alaska="ssh allen@10.0.1.22"
 
