547111bfed2dc366d0cadbdf0af6ec4d107459ba
Allen Garvey <allen.garvey@gmail.com>
2017-01-11 15:28:43 -0500

    Add alias for 'git status'

--- aliases_shared.bash
+++ aliases_shared.bash
@@ -114,6 +114,8 @@ alias gu="git pull"
 #show diff between staged files and HEAD
 alias gd="git diff --cached"
 
+alias gits="git status"
+
 #function to copy git repo and gitignore
 function git_copy() { cp -r "./.git" "$1/.git"; cp "./.gitignore" "$1/.gitignore"; }
 
