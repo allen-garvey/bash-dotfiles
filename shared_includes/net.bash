1140467f7c62f5fef105a8a7ec08ddfcd008e653
Allen Garvey <allen.garvey@gmail.com>
2017-03-07 00:00:46 -0500

    Move simple http server alias into net include file

--- shared_includes/net.bash
+++ shared_includes/net.bash
@@ -5,3 +5,7 @@ alias hosts="sudo -E vim /etc/hosts;"
 #prints out http headers for url - arg $1 is url
 #http://stackoverflow.com/questions/10060098/getting-only-response-header-from-http-post-using-curl
 function curlhead() { curl -s -D - $1 -o /dev/null; }
+
+#alias for simple python server
+#serves current directory on localhost:3000
+alias simpleserver="python -m SimpleHTTPServer 3000"
