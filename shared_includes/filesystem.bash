1bc79af2010ab414208cc6fdb34d43def596749c
Allen Garvey <allen.garvey@gmail.com>
2017-04-16 20:46:37 -0400

    Fix bug in test if file already exist when creating directory

--- shared_includes/filesystem.bash
+++ shared_includes/filesystem.bash
@@ -3,10 +3,11 @@ function makedir(){
     	>&2 echo "usage: ${FUNCNAME[0]} <directory to create>"
     	return 1;
 	fi
-	if [[ -d $1 ]]  || [[ -f $1 ]] ; then
+	if [[ -e $1 ]] ; then
     	>&2 echo "$1 already exists"
     	return 1;
 	fi
 	mkdir -p $1;
 	cd $1;
+	pwd;
 }
