32e3f2b9d47f7ff9dfacecfe04e50f05e27633a1
Allen Garvey <allen.garvey@gmail.com>
2017-04-17 22:37:00 -0400

    Remove unused 'synced' and 'mergef' functions

--- shared_includes/shell.bash
+++ shared_includes/shell.bash
@@ -14,17 +14,3 @@ function grepx() { grepc $1 $2 --include \*.$3;  }
 function findex() { find . -type f -name \*.$1; }
 #find file by name recursively in current directory
 alias findf="find . -name"
-
-#combine the contents of all files in directory matching extension into one merged file
-#$1 is input directory; $2 is full path of output file; $3 is file extension matching files in input directory
-function mergef(){ 
-	#check the output file doesn't already exist, since it would be overwritten
-	if [ -f $2 ]; then
-		(>&2 echo "Merge files aborting: output file $2 already exists")
-		kill -INT $$
-	fi
-	find $1 -name \*.$3 -print0 | xargs -0 -I file cat file > $2 
-}
-
-#alias to sync directories
-alias synced="rsync -a -p -u -v -v"
