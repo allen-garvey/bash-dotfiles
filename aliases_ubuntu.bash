49da5e6de8fb9c44851e29349addfe8e37432509
Allen Garvey <allen.garvey@gmail.com>
2017-06-26 22:33:10 -0400

    Move sshfs aliases into separate include

--- aliases_ubuntu.bash
+++ aliases_ubuntu.bash
@@ -74,31 +74,4 @@ function brightness(){
 	xrandr --output eDP1 --brightness $amount
 }
 
-#aliases for network drives
-NETWORK_DRIVES_DIR="${HOME}/Network_Drives"
-export NETWORK_DRIVES_DIR
-alias netdrives="cddirls \"$NETWORK_DRIVES_DIR\""
-#mount flip to filesystem
-alias mount_flip="sshfs garveya@access.engr.oregonstate.edu:/nfs/stak/students/g/garveya/ \"$NETWORK_DRIVES_DIR/flip\"; netdrives flip"
-
-alias mount_alaska="sshfs allen@10.0.1.22:/home/allen/ \"$NETWORK_DRIVES_DIR/alaska\"; netdrives alaska"
-
-function sshfs_unmount(){
-	#check number of arguments
-	if [[ "$#" -ne 1 ]] ; then
-    	>&2 echo "usage: ${FUNCNAME[0]} <directory_to_unmount>"
-    	return 1;
-	fi
-	target_directory="${NETWORK_DRIVES_DIR}/${1}"
-	current_directory=`pwd`
-	target_directory_regex="^${target_directory}"
-	
-	#if we are in the directory to be unmounted it will fail
-	#cannot use double quotes in regex or it will do string match instead of regex match
-	#https://stackoverflow.com/questions/19327220/bash-regex-matching-not-working
-	if [[ "$current_directory" =~ $target_directory_regex ]] ; then
-		cd ~
-	fi
-
-	fusermount -u "$target_directory"
-}
+source "${SHARED_INCLUDES_DIR}sshfs.bash"
