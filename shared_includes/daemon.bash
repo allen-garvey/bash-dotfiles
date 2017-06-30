ff60e63e06bb3dc086f20c3a8ecdbdea5f0a13f9
Allen Garvey <allen.garvey@gmail.com>
2017-06-29 23:30:42 -0400

    Add function to start process as a daemon

---
+++ shared_includes/daemon.bash
@@ -0,0 +1,10 @@
+function start_daemon(){
+	#check number of arguments
+	if [[ "$#" -ne 2 ]] ; then
+    	>&2 echo "usage: ${FUNCNAME[0]} <command> <pid_filename>"
+    	return 1;
+	fi
+	"$1" & echo $! > "$2"
+}
+
+#can stop process with `pkill -F $pid_filename`
