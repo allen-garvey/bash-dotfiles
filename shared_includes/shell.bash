becca6bd9bc4124a1adf7a4bedc1eca05d3c9afb
Allen Garvey <allen.garvey@gmail.com>
2017-07-04 23:39:22 -0400

    Refactor up alias into function that can traverse up an arbitrary number of directories

--- shared_includes/shell.bash
+++ shared_includes/shell.bash
@@ -3,7 +3,22 @@ alias a="ls -G -p"
 alias aa="a -A"
 alias aaa="aa -l -h"
 
-alias up="cd ../;a"
+function up() {
+	if [[ "$#" -ne 1 ]] ; then
+    	cd ../
+    	a
+    	return
+	fi
+	re='^[1-9][0-9]*$'
+	if ! [[ $1 =~ $re ]] ; then
+   		>&2 echo "usage: ${FUNCNAME[0]} <number_of_directories_up>"
+    	return 1;
+	fi
+	for i in $(seq 1 $1); 
+		do cd ../; 
+	done
+	a
+}
 
 #grep for pattern in files with extension
 #http://stackoverflow.com/questions/12516937/grep-but-only-certain-file-extensions
