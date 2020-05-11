7ad73ea8296a2f5f99484a5b67c24f6d0c696e3a
Allen Garvey <allengarvey@hotmail.com>
2020-05-10 22:55:15 -0400

    Update alias to update all npm dependencies to fix auditable problems

--- shared_includes/npm.bash
+++ shared_includes/npm.bash
@@ -3,6 +3,6 @@
 export PATH="~/.npm-global/bin:$PATH"
 
 # alias to update all dependencies in package.json
-alias ncuu="ncu -u && npm install"
+alias ncuu="ncu -u && npm install && npm audit fix"
 
 alias npm_reset="npm prune && npm install"
