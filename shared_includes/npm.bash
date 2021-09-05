6ffe58da6408f98eb07189f6c726b77e65c76e01
Allen Garvey <allengarvey@hotmail.com>
2021-09-05 09:43:27 -0400

    Add alias for npm install

--- shared_includes/npm.bash
+++ shared_includes/npm.bash
@@ -5,4 +5,6 @@ export PATH="~/.npm-global/bin:$PATH"
 # alias to update all dependencies in package.json
 alias ncuu="ncu -u && npm install && npm audit fix"
 
+alias npm_install="npm install && npm audit fix"
+
 alias npm_reset="npm prune && npm install"
