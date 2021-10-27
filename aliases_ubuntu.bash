fe9366586eb8b03b5a06a58817d3b13f9f2104dd
Allen Garvey <allengarvey@hotmail.com>
2021-10-27 08:48:30 -0400

    Remove empty parens from bash prompt on ubuntu when not in git repo

--- aliases_ubuntu.bash
+++ aliases_ubuntu.bash
@@ -12,9 +12,14 @@ source "${DOTFILES_DIR}aliases_shared.bash"
 PROMPT_COMMAND="set_ps1"
 
 function set_ps1(){
-	local first_part='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \[\033[00m\]('
+	local first_part='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w \[\033[00m\]'
 	local git_branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-	local last_part=')\[\033[00m\]\$ '
+
+	if [ ! -z "$git_branch" ]; then
+		git_branch="($git_branch)";
+	fi
+
+	local last_part='\[\033[00m\]\$ '
 	export PS1="$first_part$git_branch$last_part"
 }
 
