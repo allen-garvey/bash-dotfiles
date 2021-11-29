7a5a4a8be92f0f54b6cf14dd2f0b417218b851b3
Allen Garvey <allengarvey@hotmail.com>
2021-11-29 18:44:05 -0500

    Update curlhead function to use HTTPie if available

--- shared_includes/net.bash
+++ shared_includes/net.bash
@@ -4,7 +4,21 @@ alias hosts="sudo -E vim /etc/hosts;"
 
 #prints out http headers for url - arg $1 is url
 #http://stackoverflow.com/questions/10060098/getting-only-response-header-from-http-post-using-curl
-function curlhead() { curl -s -D - $1 -o /dev/null; }
+function curlhead() {
+	if [[ -z $1 ]]; then
+		>&2 echo "usage: ${FUNCNAME[0]} url"
+		return 1;
+	fi
+
+	if [ -x "$(command -v http)" ]; then
+		http $1 --head;
+	elif [ -x "$(command -v curl)" ]; then
+		curl -s -D - $1 -o /dev/null;
+	else
+		>&2 echo "curl or HTTPie required for ${FUNCNAME[0]}"
+		return 1;
+	fi
+}
 
 #alias for simple python server
 #serves current directory on localhost:3000
