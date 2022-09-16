535fed9f05f92ac37aa4c3cc29cbb03edb0c4f8b
Allen Garvey <allengarvey@hotmail.com>
2022-09-16 14:16:15 -0400

    Refactor script to generate music video to only require image file name

--- shared_includes/video.bash
+++ shared_includes/video.bash
@@ -1,18 +1,13 @@
 function generate_music_video(){
 	#check number of arguments
-	if [[ "$#" -ne 3 ]] ; then
-    	>&2 echo "usage: ${FUNCNAME[0]} <video_name> <image_file> <audio_file>"
+	if [[ "$#" -ne 1 ]] ; then
+    	>&2 echo "usage: ${FUNCNAME[0]} <image_file>"
     	return 1;
 	fi
 
-    local video_name="$1"
-    local image_file="$2"
-    local audio_file="$3"
+    local image_file="$1"
+    local audio_file=$(basename -- "$(find . -name '*.wav')")
+    local video_name="${audio_file_name%.*}"
 
-    # get audio length in seconds
-    local audio_length=$(soxi -D "$audio_file");
-    # round down audio length and add 1 second
-    local audio_length=$(awk "BEGIN { printf(\"%.0f\n\", ${audio_length}-0.5+1); }")
-	
-    time ffmpeg -r 1 -loop 1 -i "$image_file" -i "$audio_file" -acodec aac -b:a 256k -vcodec libx264 -r 1 -t "$audio_length" "${video_name}.mp4"
+    time ffmpeg -loop 1 -i "$image_file" -i "$audio_file" -c:v libx264 -tune stillimage -c:a aac -b:a 256k -pix_fmt yuv420p -shortest "${video_name}.mp4"
 }
