31c39be73ba037ca2bea9b500f06e913c3875780
Allen Garvey <allengarvey@hotmail.com>
2022-09-19 20:08:48 -0400

    Fix typo in generate music video function

--- shared_includes/video.bash
+++ shared_includes/video.bash
@@ -7,7 +7,9 @@ function generate_music_video(){
 
     local image_file="$1"
     local audio_file=$(basename -- "$(find . -name '*.wav')")
-    local video_name="${audio_file_name%.*}"
+    local video_name="${audio_file%.*}"
+
+	echo "time ffmpeg -loop 1 -i \"$image_file\" -i \"$audio_file\" -c:v libx264 -tune stillimage -c:a aac -b:a 256k -pix_fmt yuv420p -shortest \"${video_name}.mp4\""
 
     time ffmpeg -loop 1 -i "$image_file" -i "$audio_file" -c:v libx264 -tune stillimage -c:a aac -b:a 256k -pix_fmt yuv420p -shortest "${video_name}.mp4"
 }
