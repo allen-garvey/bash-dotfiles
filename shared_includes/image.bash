f9ae74b2e15ead3557b3badc3a370f79208cad53
Allen Garvey <allengarvey@hotmail.com>
2024-01-13 13:18:05 -0500

    Update convert images function to also copy existing webp images

--- shared_includes/image.bash
+++ shared_includes/image.bash
@@ -1,3 +1,3 @@
 function convert_batch_to_webp(){
-    mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -quality 85 -set filename:name '%t' 'converted/%[filename:name].webp';
+    mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -quality 85 -set filename:name '%t' 'converted/%[filename:name].webp' && find . -maxdepth 1 -type f -name \*.webp -exec cp -p {} converted \;
 }
