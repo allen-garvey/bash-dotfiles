15af75f9465a74f351363c006a30ce6adc4352bb
Allen Garvey <allengarvey@hotmail.com>
2024-01-17 10:40:25 -0500

    Add function to batch convert to jpg

--- shared_includes/image.bash
+++ shared_includes/image.bash
@@ -1,3 +1,7 @@
 function convert_batch_to_webp(){
     mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -quality 85 -set filename:name '%t' 'converted/%[filename:name].webp' && find . -maxdepth 1 -type f -name \*.webp -exec cp -p {} converted \;
 }
+
+function convert_batch_to_jpg(){
+    mkdir -p converted && find . -maxdepth 1 -type f -name \*.webp | xargs -P 4 -I@  convert @ -quality 90 -set filename:name '%t' 'converted/%[filename:name].jpg';
+}
