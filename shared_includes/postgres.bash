281598be50c8374b364dd6af8580d2118e27e569
Allen Garvey <allengarvey@hotmail.com>
2024-04-22 23:10:54 -0400

    Add postgres aliases to import data and dump data only

--- shared_includes/postgres.bash
+++ shared_includes/postgres.bash
@@ -1,9 +1,8 @@
 # shared includes for postgres
 
-
 function pg_backup(){
 	if [[ "$#" -ne 1 ]] ; then
-    	echo "usage: pg_backup database_name";
+    	echo "usage: ${FUNCNAME[0]} database_name";
     	return 1;
 	fi
 
@@ -15,4 +14,32 @@ function pg_backup(){
 	# https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
 	psql "$database_name" -c '' && \
 	pg_dump "$database_name" | gzip --best > "./${output_file}.gz"
+}
+
+function pg_backup_data(){
+	if [[ "$#" -ne 1 ]] ; then
+    	echo "usage: ${FUNCNAME[0]} database_name";
+    	return 1;
+	fi
+
+	local database_name="$1";
+	local current_timestamp=$(date -u '+%Y-%m-%d_%H-%M-%S-utc');
+	local output_file="${database_name}__${current_timestamp}__data-only.sql";
+
+	# test if database exists first
+	# https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
+	psql "$database_name" -c '' && \
+	pg_dump "$database_name" --data-only > "./${output_file}"
+}
+
+function pg_import(){
+	if [[ "$#" -ne 2 ]] ; then
+    	echo "usage: ${FUNCNAME[0]} database_name import_file.sql";
+    	return 1;
+	fi
+
+	local database_name="$1";
+	local import_file="$2";
+	
+	psql "$database_name" -v ON_ERROR_STOP=1 < "$import_file"
 }
