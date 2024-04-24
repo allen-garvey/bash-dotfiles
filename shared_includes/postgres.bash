bc7aa6b05afa3d52ef7c35947a93468e44d49143
Allen Garvey <allengarvey@hotmail.com>
2024-04-24 09:39:19 -0400

    Add postgres function to backup single schema

--- shared_includes/postgres.bash
+++ shared_includes/postgres.bash
@@ -29,7 +29,24 @@ function pg_backup_data(){
 	# test if database exists first
 	# https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
 	psql "$database_name" -c '' && \
-	pg_dump "$database_name" --data-only > "./${output_file}"
+	pg_dump "$database_name" --data-only | gzip --best > "./${output_file}.gz"
+}
+
+function pg_backup_schema(){
+	if [[ "$#" -ne 2 ]] ; then
+    	echo "usage: ${FUNCNAME[0]} database_name schema_name";
+    	return 1;
+	fi
+
+	local database_name="$1";
+	local schema_name="$2";
+	local current_timestamp=$(date -u '+%Y-%m-%d_%H-%M-%S-utc');
+	local output_file="${database_name}--${schema_name}__${current_timestamp}.sql";
+
+	# test if database exists first
+	# https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
+	psql "$database_name" -c '' && \
+	pg_dump "$database_name" --schema="$schema_name" | gzip --best > "./${output_file}.gz"
 }
 
 function pg_import(){
