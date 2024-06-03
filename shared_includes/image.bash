9932fc3ca628f44017c7368b32de7f2c5cbb9a50
Allen Garvey <allengarvey@hotmail.com>
2024-06-03 11:56:49 -0400

    Add function to batch convert to webp lossless

--- shared_includes/image.bash
+++ shared_includes/image.bash
@@ -2,6 +2,10 @@ function convert_batch_to_webp(){
     mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -quality 85 -set filename:name '%t' 'converted/%[filename:name].webp' && find . -maxdepth 1 -type f -name \*.webp -exec cp -p {} converted \;
 }
 
+function convert_batch_to_webp_lossless(){
+    mkdir -p converted && find . -maxdepth 1 -type f  \( -name '*.tif' -o -name '*.png' -o -name '*.JPG' -o -name '*.jpg' -o -name '*.jpeg' \) | xargs -P 4 -I@  convert @ -define webp:lossless=true -set filename:name '%t' 'converted/%[filename:name].webp' && find . -maxdepth 1 -type f -name \*.webp -exec cp -p {} converted \;
+}
+
 function convert_batch_to_jpg(){
     mkdir -p converted && find . -maxdepth 1 -type f -name \*.webp | xargs -P 4 -I@  convert @ -quality 90 -set filename:name '%t' 'converted/%[filename:name].jpg';
 }
