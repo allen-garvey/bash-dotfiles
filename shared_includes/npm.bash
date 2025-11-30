75659fb131dd279484871483f6335cff9306dad9
Allen Garvey <allen.garvey@gmail.com>
2025-11-29 22:15:53 -0500

    Update npm scripts to not run install scripts

--- shared_includes/npm.bash
+++ shared_includes/npm.bash
@@ -5,10 +5,10 @@ export PATH="~/.npm-global/bin:$PATH"
 # alias to update all dependencies in package.json
 function ncuu() {
     ncu -u
-    npm install
+    npm install --ignore-scripts
     npm audit fix
 }
 
-alias npm_install="npm install && npm audit fix"
+alias npm_install="npm install --ignore-scripts && npm audit fix"
 
-alias npm_reset="npm prune && npm install"
+alias npm_reset="npm prune && npm install --ignore-scripts"
